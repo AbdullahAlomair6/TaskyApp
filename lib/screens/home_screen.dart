efa5bffb879ef26a193a3cb6a750bfd7cb558da2
Abdullah <abdullah.alomair@globant.com>
2025-10-10 20:27:04 +0300

    add customScrollView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,7 @@ import 'package:tasky/screens/add_task_screen.dart';
 import 'package:tasky/widgets/task_list_widget.dart';
 
 import '../widgets/high_priority_tasks_widget.dart';
+import '../widgets/sliver_task_list_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -78,6 +79,165 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 8),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(
+                          'assets/images/profile.png',
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Good Evening ,$userName ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xffFFFCFC),
+                            ),
+                          ),
+                          Text(
+                            'One task at a time.One step closer.',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              color: Color(0xffC6C6C6),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Yuhuu ,Your work Is',
+                    style: TextStyle(
+                      color: Color(0xffFFFCFC),
+                      fontSize: 32,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'almost done ! ',
+                        style: TextStyle(
+                          color: Color(0xffFFFCFC),
+                          fontSize: 32,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SvgPicture.asset('assets/images/waving-hand.svg'),
+                    ],
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Color(0xff282828),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Achieved Tasks',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0XFFFFFCFC),
+                                ),
+                              ),
+                              Text(
+                                '$totalDoneTasks Out of $totalTasks Done',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0XFFC6C6C6),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Transform.rotate(
+                              angle: -pi / 2,
+                              child: SizedBox(
+                                height: 48,
+                                width: 48,
+                                child: CircularProgressIndicator(
+                                  value: percent / 100,
+                                  color: Color(0xff15B86C),
+                                  backgroundColor: Color(0xff6D6D6D),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "${percent.toInt()}%",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  HighPriorityTasksWidget(
+                    highPriorityList: task,
+                    onTap: (value, index) async {
+                      _doneTasks(value, index);
+                    },
+                    refresh: () => _loadTasks(),
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    'My Tasks',
+                    style: TextStyle(
+                      color: Color(0XFFFFFCFC),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                ],
+              ),
+            ),
+            isLoading
+                ? SliverToBoxAdapter(
+                    child: Center(
+                      child: CircularProgressIndicator(color: Colors.white),
+                    ),
+                  )
+                : SliverTaskListWidget(
+                    tasks: task,
+                    onTap: (value, index) {
+                      _doneTasks(value, index);
+                    },
+                  ),
+          ],
+        ),
+      ),
       floatingActionButton: SizedBox(
         height: 40,
         width: 167,
@@ -103,157 +263,6 @@ class _HomeScreenState extends State<HomeScreen> {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30),
           ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(width: 8),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/profile.png'),
-                ),
-                SizedBox(width: 8),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Good Evening ,$userName ',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                        color: Color(0xffFFFCFC),
-                      ),
-                    ),
-                    Text(
-                      'One task at a time.One step closer.',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                        color: Color(0xffC6C6C6),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Yuhuu ,Your work Is',
-              style: TextStyle(
-                color: Color(0xffFFFCFC),
-                fontSize: 32,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Row(
-              children: [
-                Text(
-                  'almost done ! ',
-                  style: TextStyle(
-                    color: Color(0xffFFFCFC),
-                    fontSize: 32,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SvgPicture.asset('assets/images/waving-hand.svg'),
-              ],
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Color(0xff282828),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Achieved Tasks',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Color(0XFFFFFCFC),
-                          ),
-                        ),
-                        Text(
-                          '$totalDoneTasks Out of $totalTasks Done',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Color(0XFFC6C6C6),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Transform.rotate(
-                        angle: -pi / 2,
-                        child: SizedBox(
-                          height: 48,
-                          width: 48,
-                          child: CircularProgressIndicator(
-                            value: percent / 100,
-                            color: Color(0xff15B86C),
-                            backgroundColor: Color(0xff6D6D6D),
-                          ),
-                        ),
-                      ),
-                      Text(
-                        "${percent.toInt()}%",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 14,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 8),
-            HighPriorityTasksWidget(
-              highPriorityList: task,
-              onTap: (value, index) async {
-                _doneTasks(value, index);
-              },
-              refresh: () => _loadTasks(),
-            ),
-            SizedBox(height: 24),
-            Text(
-              'My Tasks',
-              style: TextStyle(
-                color: Color(0XFFFFFCFC),
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            SizedBox(height: 16),
-            Expanded(
-              child: isLoading
-                  ? Center(
-                      child: CircularProgressIndicator(color: Colors.white),
-                    )
-                  : TaskListWidget(
-                      tasks: task,
-                      onTap: (value, index) {
-                        _doneTasks(value, index);
-                      },
-                    ),
-            ),
-          ],
         ),
       ),
     );
---
+++ lib/widgets/sliver_task_list_widget.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:tasky/models/task_model.dart';
+
+class SliverTaskListWidget extends StatelessWidget {
+  const SliverTaskListWidget({
+    super.key,
+    required this.tasks,
+    required this.onTap,
+    this.emptyMessage,
+  });
+
+  final List<TaskModel> tasks;
+  final String? emptyMessage;
+
+  final Function(bool? value, int? index) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return tasks.isEmpty
+        ? SliverToBoxAdapter(
+            child: Center(
+              child: Text(
+                emptyMessage ?? 'No Data',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          )
+        : SliverPadding(
+            padding: EdgeInsetsGeometry.only(bottom: 60),
+            sliver: SliverList.builder(
+              itemCount: tasks.length,
+              itemBuilder: (BuildContext context, int index) => Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Container(
+                  height: 56,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xff282828),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(width: 8),
+                      Checkbox(
+                        value: tasks[index].isDone,
+                        onChanged: (bool? value) async {
+                          onTap(value, index);
+                        },
+                        activeColor: Color(0xff15B86C),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              tasks[index].taskName,
+                              style: TextStyle(
+                                color: tasks[index].isDone
+                                    ? Color(0xffC6C6C6)
+                                    : Color(0xffFFFCFC),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                decoration: tasks[index].isDone
+                                    ? TextDecoration.lineThrough
+                                    : TextDecoration.none,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            if (tasks[index].taskDescription.isNotEmpty)
+                              Text(
+                                tasks[index].taskDescription,
+                                style: TextStyle(
+                                  color: Color(0xffC6C6C6),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: tasks[index].isDone
+                              ? Color(0XFFA0A0A0)
+                              : Color(0xffC6C6C6),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+  }
+}
